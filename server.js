1c54fffbf15fe30f55a0afd76903d59dcda280ce
SamirLama03 <117245671+SamirLama03@users.noreply.github.com>
2023-09-27 10:57:11 +0545

    post product

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const product = require("./models/productModels");
+const Product = require("./models/productModels");
 
-app.use("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.send("Hello node Api");
 });
 
-app.listen(3000, () => {
-  console.log("api with express,node and mongoDB running on Port 3000");
+app.get("/blog", (req, res) => {
+  res.send("Complete FullStack");
 });
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.msg);
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:9843722208@cluster0.4jnjaa5.mongodb.net/NodeApi?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("api with express,node and mongoDB running on Port 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
