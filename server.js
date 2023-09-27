e10aee15b4ee7d78ea988d7b3440da1546ef08c9
SamirLama03 <117245671+SamirLama03@users.noreply.github.com>
2023-09-27 13:28:51 +0545

    completed CRUD

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const product = require("./models/productModels");
 const Product = require("./models/productModels");
-
+const cors = require("cors");
+app.use(cors);
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -14,13 +15,63 @@ app.get("/blog", (req, res) => {
   res.send("Complete FullStack");
 });
 
-app.post("/product", async (req, res) => {
+app.get("/products", async (req, res) => {
+  try {
+    const products = await product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
   } catch (error) {
     console.log(error.msg);
     res.status(500).json({ msg: error.message });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot find any product with ID ${id}` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `cannot in any product with ID ${id}` });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
